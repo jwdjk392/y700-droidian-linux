d91ddb00061b91b01bc37bcddad7ebad6b88c89c
Jeongwan Bae <jeongwan.bae@outlook.com>
2026-09-09 00:56:39 +0900

    Debian configuration for Droidian and Y700

---
+++ debian/linux-headers-5.10.264-lenovo-tb320fc/usr/src/linux-headers-5.10.264-lenovo-tb320fc/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Sep 6 17:10:51 UTC 2026"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "f160e1153518"
+#define LINUX_COMPILER "Android (7284624, based on r416183b) clang version 12.0.5 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee), LLD 12.0.5 (/buildbot/src/android/llvm-toolchain/out/llvm-project/lld c935d99d7cf2016289302412d708641d52d2f7ee)"
